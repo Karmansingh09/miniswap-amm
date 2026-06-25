ad045f0d1886f8a3d02c8b8b25c82337fff4fbfa
Karman Singh Chandhok <233904263+Karmansingh09@users.noreply.github.com>
2026-06-25 11:45:03 +0530

    feat: setup Express server and add liquidity pool model

---
+++ backend/src/models/liquidityPool.js
@@ -0,0 +1,12 @@
+const liquidityPool = {
+  // The amount of the token asset currently held in the pool.
+  assetReserve: 1000,
+
+  // The amount of USDC currently held in the pool.
+  usdcReserve: 200000,
+
+  // The total supply of LP tokens that represent liquidity provider ownership.
+  lpTokenSupply: 1000,
+};
+
+module.exports = liquidityPool;
